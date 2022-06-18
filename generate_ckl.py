8609220c9710d21ebb7821aad25dc11827654ad4
brad <bradleynielsen@gmail.com>
2022-06-18 11:34:46 -0500

    input csv working, merging into main

--- generate_ckl.py
+++ generate_ckl.py
@@ -86,6 +86,7 @@ for root, dirs, files in os.walk(xccdfdir):
                                             cklRuleID      = element_stigdata[1].text   #get the CKL Rule ID                                        
                                             #When both IDs are found in the CKL VULN...
                                             if (cklVulnID !="" and cklRuleID != ""):
+                                                #print("Updating "+xccdf_host_name+" CKL "+cklVulnID+" with data from XCCDF")
                                                 #init the vars:
                                                 cklStatus               = ""
                                                 finding_details         = ""
@@ -104,7 +105,7 @@ for root, dirs, files in os.walk(xccdfdir):
                                                                 resultValue = resultchild.text
                                                                 if (resultValue == "pass"):
                                                                     cklStatus = "NotAFinding"
-                                                                    print(cklStatus)
+                                                                    #print(cklStatus)
                                                                 if (resultValue == "fail"):
                                                                     cklStatus = "Open"
                                                                     try:
@@ -112,7 +113,7 @@ for root, dirs, files in os.walk(xccdfdir):
                                                                         for row in trackercsv:
                                                                             trackerVul_ID = (row["Vul_ID"])
                                                                             if (trackerVul_ID == cklVulnID):
-                                                                                print("tracker ID: " + trackerVul_ID +"CKL ID" + cklVulnID)
+                                                                                #print("tracker ID: " + trackerVul_ID +"CKL ID" + cklVulnID)
                                                                                 #Set the Finding details and comments from the tracker csv
                                                                                 trackerFinding_Details = "\n Additional Info: \n"+(row["Finding_Details"])                                                                        
                                                                                 trackerComments        = (row["Comments"])                                                                    
@@ -134,81 +135,32 @@ for root, dirs, files in os.walk(xccdfdir):
                                                                 element_VULN.find('STATUS').text                 = cklStatus                                                
                                                                 element_VULN.find('FINDING_DETAILS').text        = finding_details
                                                                 element_VULN.find('COMMENTS').text               = trackerComments                                                                                                                   
-                                                                break                                                        
-                                                #XCCDF has no results, try the csv
-                                                if (xccdfValueUsed == False):                                                
-                                                    try:
-                                                        summarycsv = csv.DictReader(open(summarycsvpath))
-                                                        summaryValueUsed = False
-                                                        #loop over the summary
-                                                        for row in summarycsv:                                                        
-                                                            if (row['Computer         ']) == xccdf_host_name:
-                                                                try:
-                                                                    if row[cklVulnID] == "Pass":                                                                
-                                                                        print("Updating "+xccdf_host_name+" CKL "+cklVulnID+" with data from Summary CSV", end = '')
-                                                                        trackerUsed = False
-                                                                        count += 1
-                                                                        element_VULN.find('STATUS').text = "NotAFinding"                                                                    
-                                                                        trackercsv = csv.DictReader(open(trackercsvpath))                                
-                                                                        for row in trackercsv:
-                                                                            print("tracker ID: " + trackerVul_ID +"CKL ID" + cklVulnID)
-                                                                                
-                                                                            # get the finding details and comments value from the tracker csv
-                                                                            trackerVul_ID          = (row["Vul_ID"])
-                                                                            trackerFinding_Details = (row["Finding_Details"])
-                                                                            trackerComments        = (row["Comments"])
-                                                                            trackerInitial_Status  = (row["Initial_Status"])
-                                                                            trackerSet_Status      = (row["Set_Status"])                                                                    
-                                                                            if trackerVul_ID != cklVulnID:
-                                                                                continue
-                                                                            if trackerVul_ID == cklVulnID:                                                                                                                                               
-                                                                                element_VULN.find('FINDING_DETAILS').text        = trackerFinding_Details
-                                                                                element_VULN.find('COMMENTS').text               = trackerComments
-                                                                                trackerUsed = True                                                                                                                                           
-                                                                            summaryValueUsed = True
-                                                                    if row[cklVulnID] == "Fail":
-                                                                        #print("Updating "+xccdf_host_name+" CKL "+cklVulnID+" with data from Summary CSV", end = '')
-                                                                        trackerUsed = False
-                                                                        element_VULN.find('STATUS').text = "Open"
-                                                                        #if the summary status is fail, get the value from the tracker csv
-                                                                        trackercsv = csv.DictReader(open(trackercsvpath))                                
-                                                                        for row in trackercsv:
-                                                                            trackerVul_ID          = (row["Vul_ID"])
-                                                                            trackerFinding_Details = (row["Finding_Details"])
-                                                                            trackerComments        = (row["Comments"])
-                                                                            trackerInitial_Status  = (row["Initial_Status"])
-                                                                            trackerSet_Status      = (row["Set_Status"])
-                                                                            if trackerVul_ID != cklVulnID:
-                                                                                continue
-                                                                            if trackerVul_ID == cklVulnID:                                                                        
-                                                                                element_VULN.find('STATUS').text                 = trackerSet_Status                                                
-                                                                                element_VULN.find('FINDING_DETAILS').text        = trackerFinding_Details
-                                                                                element_VULN.find('COMMENTS').text               = trackerComments
-                                                                                trackerUsed = True                                                                        
-                                                                except:
-                                                                    #nothing found in summary csv
-                                                                    
-                                                                    #if the summary status is fail, get the value from the tracker csv
-                                                                    trackercsv = csv.DictReader(open(trackercsvpath))                                
-                                                                    for row in trackercsv:
-                                                                        trackerVul_ID          = (row["Vul_ID"])
-                                                                        trackerFinding_Details = (row["Finding_Details"])
-                                                                        trackerComments        = (row["Comments"])
-                                                                        trackerInitial_Status  = (row["Initial_Status"])
-                                                                        trackerSet_Status      = (row["Set_Status"])
-                                                                        if trackerVul_ID != cklVulnID:
-                                                                            continue
-                                                                        if trackerVul_ID == cklVulnID:                                                                        
-                                                                            element_VULN.find('STATUS').text                 = trackerSet_Status                                                
-                                                                            element_VULN.find('FINDING_DETAILS').text        = trackerFinding_Details
-                                                                            element_VULN.find('COMMENTS').text               = trackerComments
-                                                                            trackerUsed = True
-                                                                            
+                                                                break
 
+                                                #XCCDF has no results, try the csv                                                
+                                                if (xccdfValueUsed == False):                                                    
+                                                    try:                                                        
+                                                        #get the value from the tracker csv
+                                                        trackercsv = csv.DictReader(open(trackercsvpath))                                
+                                                        for row in trackercsv:
+                                                            trackerVul_ID          = (row["Vul_ID"])
+                                                            trackerFinding_Details = (row["Finding_Details"])
+                                                            trackerComments        = (row["Comments"])
+                                                            trackerInitial_Status  = (row["Initial_Status"])
+                                                            trackerSet_Status      = (row["Set_Status"])
+                                                            if trackerVul_ID != cklVulnID:
+                                                                continue
+                                                            if trackerVul_ID == cklVulnID:                                                                        
+                                                                element_VULN.find('STATUS').text                 = trackerSet_Status                                                
+                                                                element_VULN.find('FINDING_DETAILS').text        = trackerFinding_Details
+                                                                element_VULN.find('COMMENTS').text               = trackerComments
+                                                                trackerUsed = True
+                                                                print("filling in tracker info")                                                            
                                                     except:
-                                                        #summarycsv failed to load
-                                                        
+                                                        #tracker failed to load
+                                                        print("tracker failed to load")                                                            
                                                         continue
+                                                    
                             else:
                                 #iStig child element is not 'VULN'
                                 continue
